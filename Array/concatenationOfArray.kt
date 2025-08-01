c533284a7af143e8ac9ac1de5934aa6946f19f4b
RakibRahman <md.rr.talukder@gmail.com>
2025-08-01 16:09:48 +0600

    LeetCode 1929 – Concatenation of Array

--- Array/concatenationOfArray.kt
+++ Array/concatenationOfArray.kt
@@ -1,3 +1,23 @@
+class Solution {
+    fun getConcatenation(nums: IntArray): IntArray {
+        return  nums+nums
+    }
+
+    fun getConcatenationV2(nums: IntArray): IntArray {
+        val size = nums.size
+        val result= IntArray(2*size)
+        for(i in 0 until size){
+            result[i] = nums[i]
+            result[i+size] = nums[i]
+        }
+
+        return result
+    }
+}
+
+
+
 fun main(){
-    println("rakib")
+   val output = Solution().getConcatenationV2(intArrayOf(1,2,1)).toList()
+    println(output)
 }
